af95bcb306c4df59bf0dbf4da950f284f9d2e543
Andrew <andrewstaveley14@gmail.com>
2023-12-11 18:01:02 -0500

    Completed Assignmnet

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,13 +1,33 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar.js";
+import { useParams } from "react-router-dom";
 
 function Movie() {
+  const [movie, setMovie] = useState(["Loading Movie..."])
+  const [movieGenres, setMovieGenres] = useState(["Loading Genres..."])
+  const params = useParams();
+  const movieId = params.id;
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/movies/${movieId}`)
+    .then((r) => r.json())
+    .then((data) => {
+      setMovie(data)
+      setMovieGenres(data.genres)
+    })
+  }, []);
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        {<NavBar />}
       </header>
       <main>
-        {/* Movie info here! */}
+        <h1>{movie.title}</h1>
+        <p>Length: {movie.time} Minutes</p>
+        {movieGenres.map((genre) => {
+          return <span key={genre}>{genre}</span>
+        })}
       </main>
     </>
   );
